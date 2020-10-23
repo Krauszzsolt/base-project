153bd119c7d56f56e6de3fdc4c654697123585fa
Horváth Dénes <horvathdes97@gmail.com>
2020-10-23 13:20:34 +0200

    backend initial create

---
+++ base-backend/Backend/API/Controllers/TestController.cs
@@ -0,0 +1,33 @@
+﻿using API.Attributes;
+using API.Controllers.Base;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Backend.Controllers
+{
+
+    public class TestController : BaseController
+    {
+
+        /// <summary>
+        /// Test API without authentication.
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet("noauth")]
+        public ActionResult<string> TestNoauth()
+        {
+            return Ok("API is working.");
+        }
+
+        /// <summary>
+        /// Test API with authentication.
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet("auth")]
+        [Authorize]
+        public ActionResult<string> TestAuth()
+        {
+            return Ok($"Authenticated. Hello {CurrentUser.UserName}!");
+        }
+
+    }
+}
